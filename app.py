75edcb60a6ee1d04f2fdbf744fcfa84a4154d902
Ursavin <donovankhoun@gmail.com>
2024-10-15 12:29:39 +0000

    Backend API

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 from flask import Flask, render_template
+from flask import Flask, request, jsonify
+from flask_restful import Api, Resource
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from flask_migrate import Migrate
 
+from models import db, User
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+app = Flask(__name__)
+# applies CORS headers to all routes, enabling resources to be accessed
+CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+migrate = Migrate(app, db)
+db.init_app(app)
+
+db = SQLAlchemy(app)
+api = Api(app)
