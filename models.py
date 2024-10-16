28efe474158a10f890be8eb645aa6c0b9c72b65c
Ursavin <donovankhoun@gmail.com>
2024-10-16 15:14:45 +0000

    Rewriting so the code is cohesive

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ db = SQLAlchemy()
 
 class User(db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True) 
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
 
@@ -19,7 +19,7 @@ class User(db.Model):
     
 class Battery(db.Model):
     __tablename__ = 'batteries'
-    id = db.Column(db.Integer, primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True)
     status = db.Column(db.String(25), unique=True, nullable=False)
     type = db.Column(db.String(50), unique=True, nullable=False)
     coordinates: Mapped[list["Coordinates"]] = relationship(back_populates = "battery")
@@ -30,12 +30,12 @@ class Battery(db.Model):
             'status': self.status
         }
 
+# TODO: We should add a category for the type of damage it has suffered.
+# This will help refine the model and aid the enduser.
+# In the application layer, we need to implement the code that assigns an enum/string.  
 class Coordinates(db.Model):
     __tablename__ = 'coordinates'
     id: Mapped[int] = mapped_column(primary_key=True)
     batteryId: Mapped[int] = mapped_column(ForeignKey("batteries.id"))
     battery: Mapped["Battery"] = relationship(back_populates="coordinates")
-    # TODO: We should add a category for the type of damage it has suffered.
-    # This will help refine the model and aid the enduser.
-    # In the application layer, we need to implement the code that assigns an enum/string. 
-    
+ 
