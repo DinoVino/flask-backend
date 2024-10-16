41a5eb355a6391849b23ccbe49c104bcd91a32af
Ursavin <donovankhoun@gmail.com>
2024-10-16 14:43:02 +0000

    Added the coordinates with a one to many relationship

--- models.py
+++ models.py
@@ -1,4 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKey, Integer
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase, relationship
 
 db = SQLAlchemy()
 
@@ -19,6 +21,8 @@ class Battery(db.Model):
     __tablename__ = 'batteries'
     id = db.Column(db.Integer, primary_key=True)
     status = db.Column(db.String(25), unique=True, nullable=False)
+    type = db.Column(db.String(50), unique=True, nullable=False)
+    coordinates: Mapped[list["Coordinates"]] = relationship(back_populates = "battery")
 
     def to_dict(self):
         return{
@@ -26,3 +30,8 @@ class Battery(db.Model):
             'status': self.status
         }
 
+class Coordinates(db.Model):
+    __tablename__ = 'coordinates'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    batteryId: Mapped[int] = mapped_column(ForeignKey("batteries.id"))
+    battery: Mapped["Battery"] = relationship(back_populates="coordinates")
